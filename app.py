14f1ca98598aa125f991d87619e625d0e59b31a1
Julio Rodriguez <juliorod63@gmail.com>
2025-08-19 20:36:42 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,97 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import plotly.figure_factory as ff
+import numpy as np
+import spacy
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
+from textblob import TextBlob
+
+from utils import load_data, limpiar_texto, transformacion_df, calcular_NPS_Alexia, calcular_NPS_Modulo, transformacion_df_comentarios
+nlp = spacy.load("es_core_news_sm")
+
+st.set_page_config(
+    page_title="Resultados Encuesta Satisfacción Clientes Chile",
+    page_icon=":bar_chart:",
+    layout="wide"
+)
+
+st.title("Resultados Encuesta Satisfacción Clientes Chile")
+st.write("Bienvenido a la aplicación de Resultados Encuesta Satisfacción Clientes Chile.")
+
+password_guess = st.text_input("Ingrese la contraseña para acceder a los resultados:", type="password")
+
+if password_guess != st.secrets["password"]:
+    st.write("Contraseña incorrecta. Inténtalo de nuevo.")
+    st.stop()
+else:
+    st.success("Contraseña correcta. Acceso concedido.")
+
+#url del archivo
+file_path = "https://raw.githubusercontent.com/juliorod63/DATASETS/refs/heads/main/CL_Encuesta.csv"
+df = load_data(file_path)
+
+st.markdown("### Resultados de la Encuesta")
+st.write(" Respuestas: ", df.shape[0])
+df = transformacion_df(df)
+
+st.dataframe(df)
+
+st.divider()
+col1, col2 = st.columns(2)
+col1.metric(label="NPS Alexia", value=f"{calcular_NPS_Alexia(df):.2f}")
+col2.metric(label="NPS Modulo", value=f"{calcular_NPS_Modulo(df):.2f}")
+st.divider()
+
+
+st.plotly_chart(px.histogram(df, x="NPS_Recomendar", color="Cargo", title="Distribución de NPS x Cargo"))
+
+st.plotly_chart(px.histogram(df, x="NPS_Recomendar", color="Modulo_Usado", title="Distribución de NPS x Modulo"))
+
+st.plotly_chart(px.histogram(df, x="CS_Alexia", color="Modulo_Usado", title="Distribución de CS x Modulo"))
+
+st.plotly_chart(px.histogram(df, x="Cargo", color="Modulo_Usado", title="Distribución x Cargo x Modulo"))
+
+st.plotly_chart(px.histogram(df, x="NPS_Modulo", color="Antiguedad", title="Distribución por NPS Modulo y Antiguedad"))
+
+# Selecciona las columnas numéricas que quieres comparar
+cols = ["CS_Alexia", "NPS_Modulo", "NPS_Recomendar", "Satisf_Modulo"]  # ajusta según tus datos
+
+st.markdown("### Matriz de Dispersión")
+fig = ff.create_scatterplotmatrix(df[cols], diag='box',height=800, width=800)
+st.plotly_chart(fig, use_container_width=True)
+
+st.markdown("### Analisis de comentarios - Nube de Palabras (tokenizadas)")
+# Aplica la función a los comentarios
+df_comentarios = transformacion_df_comentarios(df)
+df_comentarios_limpios = df_comentarios.apply(limpiar_texto)
+ 
+# Genera la nube de palabras
+texto_unido = " ".join(df_comentarios_limpios)
+# Genera la nube de palabras
+wordcloud = WordCloud(width=800, height=400, background_color='white').generate(texto_unido)
+fig, ax = plt.subplots(figsize=(10, 5))
+ax.imshow(wordcloud, interpolation='bilinear')
+ax.axis('off')
+
+# Muestra la imagen en Streamlit
+st.pyplot(fig)
+
+
+st.markdown("### Análisis de NPS por Variables")
+# Supón que df es tu DataFrame ya cargado y transformado
+variables = ["Cargo", "Antiguedad", "Centro", "Modulo_Usado"]  # agrega las variables que quieras analizar
+
+opcion = st.selectbox("Selecciona una variable para analizar NPS_Recomendacion:", variables)
+
+# Gráfico de distribución de NPS_Recomendacion según la variable seleccionada
+fig = px.box(df, x=opcion, y="NPS_Recomendar", title=f"NPS_Recomendacion según {opcion}")
+st.plotly_chart(fig)
+
+
+
+
+
+
+
---
+++ utils.py
@@ -0,0 +1,153 @@
+import pandas as pd
+import streamlit as st
+import matplotlib.pyplot as plt
+import spacy
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+from textblob import TextBlob
+
+nlp = spacy.load("es_core_news_sm")
+
+def load_data(file_path):
+    df = pd.read_csv(file_path)
+    return df
+
+def plot_results(df):
+    st.bar_chart(df["satisfaction_rating"]) 
+
+def calcular_NPS_Modulo(df):
+    promoters = df[df["NPS_Modulo"] >= 9].shape[0]
+    detractors = df[df["NPS_Modulo"] <= 6].shape[0]
+    total_responses = df["NPS_Modulo"].shape[0]
+    if total_responses == 0:
+        return 0
+    return (promoters - detractors) / total_responses * 100
+
+def calcular_NPS_Alexia(df):
+    promoters = df[df["NPS_Recomendar"] >= 9].shape[0]
+    detractors = df[df["NPS_Recomendar"] <= 6].shape[0]
+    total = df["NPS_Recomendar"].shape[0]
+    print (total, promoters, detractors)
+    nps = ((promoters - detractors) / total) * 100
+    print(nps)
+    if total == 0:
+        return 0
+    return nps
+
+def transformacion_df(df):
+
+    df.rename(columns={df.columns[1]: "email", df.columns[2]:"Nombre", df.columns[3]:"Centro", df.columns[4]:"Cargo", df.columns[5]:"Antiguedad", df.columns[6]:"Modulo_Usado",df.columns[7]:"Satisf_Modulo",df.columns[8]:"NPS_Modulo",df.columns[9]:"Capacitacion",df.columns[10]:"CS_Alexia", df.columns[11]:"Funcionalidad_Alexia", df.columns[12]:"Amigable_Alexia",df.columns[13]:"NPS_Recomendar", df.columns[14]:"Mejoras"}, inplace=True)
+    df = df.drop(columns=[df.columns[0]])
+
+    df["Cargo"] = df["Cargo"].str.lower().replace({
+    "profesor": "Docente",
+    "profesor ": "Docente",
+    "profesora ": "Docente",
+    "profrsora": "Docente",
+    "docente ": "Docente",
+    "profesora": "Docente",
+    "maestro": "Docente",
+    "maestra": "Docente",
+    "docente artes visuales": "Docente",
+    "profesora de química y biología": "Docente",
+    "profesor y apoderado": "Docente",
+    "coeducadora 1° basico": "Docente",
+    "educador": "Docente",
+    "educadora": "Docente",
+    "educadora diferencial": "Docente",
+    "educadora de párvulos": "Docente",
+    "educadora basica": "Docente",
+    "educadora básica": "Docente"
+    # Agrega aquí más variantes según lo que observes en los datos
+    })
+    df.loc[df["Cargo"].str.contains("jefe"), "Cargo"] = "Jefe/a"
+    df.loc[df["Cargo"].str.contains("jefa"), "Cargo"] = "Jefe/a"
+    df.loc[df["Cargo"].str.contains("coordinador"), "Cargo"] = "Coordinador/a"
+    df.loc[df["Cargo"].str.contains("coordinación"), "Cargo"] = "Coordinador/a"
+    df.loc[df["Cargo"].str.contains("asistente"), "Cargo"] = "Asistente"
+    df.loc[df["Cargo"].str.contains("secretaría"), "Cargo"] = "Secretaria"
+    df.loc[df["Cargo"].str.contains("secretaria"), "Cargo"] = "Secretaria"
+    df.loc[df["Cargo"].str.contains("encargado"), "Cargo"] = "Encargado"
+    df.loc[df["Cargo"].str.contains("encargada"), "Cargo"] = "Encargado"
+    df.loc[df["Cargo"].str.contains("administrativo"), "Cargo"] = "Administrativo"
+    df.loc[df["Cargo"].str.contains("administrativa"), "Cargo"] = "Administrativo"
+    df.loc[df["Cargo"].str.contains("psicologo"), "Cargo"] = "Psicologo/a"
+    df.loc[df["Cargo"].str.contains("psicólogo"), "Cargo"] = "Psicologo/a"
+    df.loc[df["Cargo"].str.contains("psicologa"), "Cargo"] = "Psicologo/a"
+    df.loc[df["Cargo"].str.contains("psicóloga"), "Cargo"] = "Psicologo/a"
+    df.loc[df["Cargo"].str.contains("psicopedagoga"), "Cargo"] = "Psicopedagogo/a"
+    df.loc[df["Cargo"].str.contains("psicopedagogo"), "Cargo"] = "Psicopedagogo/a"
+    df.loc[df["Cargo"].str.contains("orientador"), "Cargo"] = "Orientador/a"
+    df.loc[df["Cargo"].str.contains("orientadora"), "Cargo"] = "Orientador/a"
+    df.loc[df["Cargo"].str.contains("profesor jefe"), "Cargo"] = "Jefe/a"
+    df.loc[df["Cargo"].str.contains("inspector"), "Cargo"] = "Inspector/a"
+    df.loc[df["Cargo"].str.contains("inspectora"), "Cargo"] = "Inspector/a"
+    df.loc[df["Cargo"].str.contains("profesional de apoyo"), "Cargo"] = "Profesional de Apoyo"
+    df.loc[df["Cargo"].str.contains("docente"), "Cargo"] = "Docente"
+    df.loc[df["Cargo"].str.contains("profesor de"), "Cargo"] = "Docente"
+    df.loc[df["Cargo"].str.contains("profesora de"), "Cargo"] = "Docente"
+    df.loc[df["Cargo"].str.contains("profesor"), "Cargo"] = "Docente"
+    df.loc[df["Cargo"].str.contains("monitor"), "Cargo"] = "Inspector"
+    df.loc[df["Cargo"].str.contains("monitora"), "Cargo"] = "Inspector"
+    df.loc[df["Cargo"].str.contains("gerente"), "Cargo"] = "Gerente"
+    df.loc[df["Cargo"].str.contains("directora"), "Cargo"] = "Director/a"
+    df.loc[df["Cargo"].str.contains("director"), "Cargo"] = "Director/a"
+    df.loc[df["Cargo"].str.contains("dirección"), "Cargo"] = "Director/a"
+    df.loc[df["Cargo"].str.contains("encargada de convivencia escolar"), "Cargo"] = "Encargado"
+    df.loc[df["Cargo"].str.contains("encargada de convivencia escolar"), "Cargo"] = "Encargado"
+    df.loc[df["Cargo"].str.contains("encargado de convivencia escolar"), "Cargo"] = "Encargado"
+    df.loc[df["Cargo"].str.contains("subdirector"), "Cargo"] = "Director/a"
+    df.loc[df["Cargo"].str.contains("subdirectora"), "Cargo"] = "Director/a"
+    df.loc[df["Cargo"].str.contains("encargada de biblioteca"), "Cargo"] = "Bibliotecario/a"
+    df.loc[df["Cargo"].str.contains("encargado de biblioteca"), "Cargo"] = "Bibliotecario/a"
+    df.loc[df["Cargo"].str.contains("bibliotecaria"), "Cargo"] = "Bibliotecario/a"
+    df.loc[df["Cargo"].str.contains("bibliotecario"), "Cargo"] = "Bibliotecario/a"
+    df.loc[df["Cargo"].str.contains("técnico"), "Cargo"] = "Técnico/a"
+    df.loc[df["Cargo"].str.contains("técnica"), "Cargo"] = "Técnico/a"
+    df.loc[df["Cargo"].str.contains("convivencia"), "Cargo"] = "Convivencia"
+    df.loc[df["Cargo"].str.contains("consejero"), "Cargo"] = "Consejero/a"
+    df.loc[df["Cargo"].str.contains("consejera"), "Cargo"] = "Consejero/a"
+    df.loc[df["Cargo"].str.contains("encargada de recursos"), "Cargo"] = "Encargado"
+    df.loc[df["Cargo"].str.contains("educador"), "Cargo"] = "Docente"
+    df.loc[df["Cargo"].str.contains("enfermer"), "Cargo"] = "Enfermero/a"
+    df.loc[df["Cargo"].str.contains("apoderad"), "Cargo"] = "Apoderado/a"
+    df.loc[df["Cargo"].str.contains("teacher"), "Cargo"] = "Docente"
+    df.loc[df["Cargo"].str.contains("utp"), "Cargo"] = "Coordinador/a"
+    df.loc[df["Cargo"].str.contains("asesor"), "Cargo"] = "Asesor/a"
+    df.loc[df["Cargo"].str.contains("maestra"), "Cargo"] = "Docente"
+    df.loc[df["Cargo"].str.contains("subdirección"), "Cargo"] = "Director/a"
+    df.loc[df["Cargo"].str.contains("jede"), "Cargo"] = "Jefe/a"
+    df.loc[df["Cargo"].str.contains("jefe de"), "Cargo"] = "Jefe/a"
+    df.loc[df["Cargo"].str.contains("ed."), "Cargo"] = "Docente"
+    df.loc[df["Cargo"].str.contains("recretaria"), "Cargo"] = "Secretaria"
+    df.loc[df["Cargo"].str.contains("soporte"), "Cargo"] = "Soporte Sistemas"
+    df.loc[df["Cargo"].str.contains("porter"), "Cargo"] = "Mantenimiento"
+    df.loc[df["Cargo"].str.contains("mantencion"), "Cargo"] = "Mantenimiento"
+    df.loc[df["Cargo"].str.contains("mantenimiento"), "Cargo"] = "Mantenimiento"
+    df.loc[df["Cargo"].str.contains("prof de"), "Cargo"] = "Docente"
+    df.loc[df["Cargo"].str.contains("eca"), "Cargo"] = "Encargado/a"
+    counts = (df["Cargo"].value_counts())
+
+    # Reemplaza los valores que aparecen solo una vez por "Otros"
+    df["Cargo"] = df["Cargo"].apply(lambda x: x if counts[x] >  2 else "Otros")
+    return df
+
+
+def transformacion_df_comentarios(df):
+    df_comentarios = df[df["Mejoras"].notna() & (df["Mejoras"] != "")]
+    df_comentarios = df_comentarios["Mejoras"]
+    df_comentarios = df_comentarios.str.lower().str.replace("[-_ . , ]", " ", regex=True)
+    return df_comentarios
+
+def tokenizar(df_comentarios):
+    nlp = spacy.load("es_core_news_sm")
+    doc = nlp(" ".join(df_comentarios))
+    return [token.text for token in doc if not token.is_stop and not token.is_punct]
+
+
+# Función para limpiar texto: tokenizar y eliminar stop words y signos de puntuación
+def limpiar_texto(texto):
+    doc = nlp(texto)
+    tokens = [token.lemma_ for token in doc if not token.is_stop and not token.is_punct]
+    return " ".join(tokens)
+
